648873dca3e0d18994b74a4d62a2dfbb5067dc62
HenriqueChepli <henriquechepli@gmail.com>
2023-08-24 16:32:18 -0300

    ahhhhhh

--- java/global.js
+++ java/global.js
@@ -24,3 +24,18 @@ AllLinks.forEach((elem) => {
     });
   });
 });
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 4000)
+
+function nextImage(){
+  count ++;
+  if(count>4){
+    count = 1;
+  }
+  document.getElementById("radio"+count).checked = true;
+}
